7638122c31bf2161d166f4c868348624fb0e18d7
Dantcar <decio.fatec2016@gmail.com>
2017-01-31 14:09:05 -0200

    31-01-2017 casa

---
+++ ImagemScreen/src/OnlyExt.java
@@ -0,0 +1,27 @@
+
+import java.io.File;
+import java.io.FilenameFilter;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Décio
+ */
+class OnlyExt implements FilenameFilter {
+  String ext;
+
+  public OnlyExt(String ext) {
+    this.ext = "." + ext;
+  }
+
+  @Override
+  public boolean accept(File dir, String name) {
+    return name.endsWith(ext);
+  }
+  
+}
