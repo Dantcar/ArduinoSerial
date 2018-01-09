aa45487d8d11f8bd11918a0544ec9701fee62b82
decio_000 <decio_000@DacPc2013>
2018-01-09 12:48:55 -0200

    09-01-2018

---
+++ clock/src/Clock.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Décio
+ */
+public class Clock extends javax.swing.JFrame{;
+    int timeRun =0;
+    
+       public static void main(String[] args)
+       {
+          
+          System.out.printf("%s \n", "oi oi oi");
+       }
+   
+    
+}
